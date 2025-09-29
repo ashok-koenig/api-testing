06a576a29991faf34b1421bebb0ba24850d74ee7
Ashok S <ashok.s@koenig-solutions.com>
2025-09-29 14:51:54 +0530

    data types

---
+++ Day-1/01-Java-Basics/src/DataTypeDemo.java
@@ -0,0 +1,15 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        System.out.println("Working with data types and variables");
+
+        int age = 25;
+        String name = "John";
+        double salary = 23457.50;
+        boolean isManager = true;
+
+        System.out.println("Age: "+ age);
+        System.out.println("Name: "+ name);
+        System.out.println("Salary: "+ salary);
+        System.out.println("isManager: "+ isManager);
+    }
+}
